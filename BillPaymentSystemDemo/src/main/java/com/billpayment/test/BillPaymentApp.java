063c0eaef4c1c106cfc79206911c8252e658b17c
Joshua Julian <Joshua Julian@DESKTOP-1V5B885>
2018-04-27 22:35:34 +0530

    FirstCommit-BillPaymentSystemDemo

---
+++ BillPaymentSystemDemo/src/main/java/com/billpayment/test/BillPaymentApp.java
@@ -0,0 +1,14 @@
+package com.billpayment.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillPaymentApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillPaymentApp.class, args);
+
+	}
+
+}
